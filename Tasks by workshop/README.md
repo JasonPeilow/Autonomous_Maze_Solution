fdfaa224c25588890abdcbb0556018f49dc2e1bf
Jason Peilow <97916214+JasonPeilow@users.noreply.github.com>
2022-03-29 11:14:56 +0100

    Add files via upload

---
+++ Tasks by workshop/README.md
@@ -0,0 +1,30 @@
+# cmp3103_amr_19-20
+CMP3103: Autonomous Mobile Robotics - Workshop Tasks
+
+List of ROS Packages
+
+http://repositories.ros.org/status_page/ros_kinetic_default.html
+
+WS4 code (opt task) in action:
+
+https://youtu.be/Fvym6sQEhFg
+
+Go to this repo for every ROS book known to man (That is decent):
+
+https://github.com/StevenShiChina/books
+
+Also, found Probabilistic Robotics as a PDF:
+
+https://docs.ufpr.br/~danielsantos/ProbabilisticRobotics.pdf
+
+And Autonomous Mobile Robotics:
+
+http://home.deib.polimi.it/gini/robot/docs/siegwart.pdf
+
+You may know The Construct (Tutorials for ROS). The channel is great if you have not see it...
+
+https://www.youtube.com/channel/UCt6Lag-vv25fTX3e11mVY1Q
+
+...as is their Bitbucket. Good to use alongside tutorials.
+
+https://bitbucket.org/%7B33781e18-852e-47c2-af39-759234f1f214%7D/
---
+++ Tasks by workshop/ws1_and_ws2/ws2_task2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist # This is the message type the robot uses for velocities
+
+
+class CommandVelocity():
+	"""Driving my robot
+	"""
+
+	def __init__(self):
+		rospy.loginfo("Starting node")
+		self.pub = rospy.Publisher("/cmd_vel_mux/input/navi", Twist) # 
+
+	# A function to send velocities until the node is killed
+	def send_velocities(self):
+		r = rospy.Rate(10) # Setting a rate (hz) at which to publish
+		while not rospy.is_shutdown(): # Running until KILLED
+			rospy.loginfo("Sending commands")
+			twist_msg = Twist() # Creating a new message to send to the robot
+
+			twist_msg.linear.x = 0.25 # expand the circle (Max of 0.6 velocity plz)
+			twist_msg.angular.z = 0.5 # just rotate. Without velocity it would just spin.
+
+			# rotate on the spot or drive in a circle?
+
+			self.pub.publish(twist_msg) # Sending the message via our publisher
+			r.sleep() # Calling sleep to ensure the rate we set above
+
+if __name__ == '__main__':
+	rospy.init_node("command_velocity")
+	cv = CommandVelocity()
+	cv.send_velocities() # Calling the method
+	rospy.spin()
